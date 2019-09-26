9420db24d4a7f93675ad2bff40df60e7051b1ec8
Jeff Walker <walker@maine.edu>
2019-09-26 09:50:51 -0400

    major book update

--- R/clean_label.R
+++ R/clean_label.R
@@ -1,6 +1,11 @@
 # function to remove spaces and units from column labels
 # Jeff Walker
 # November 12, 2018
+# to use, add "source("clean_label.R")"
+
+odd <- function(x) x%%2 != 0
+
+even <- function(x) x%%2 == 0
 
 clean_label <- function(x){
   # clean units in parentheses and units with space in front of parenthesis
@@ -17,11 +22,26 @@ clean_label <- function(x){
   # replace % with "perc"
   x <- gsub("%", "perc_", x)
 
+  # replace hyphens
+  x <- gsub("-", "_", x)
+
+  # replace forward slash
+  x <- gsub("/_", ".", x)
+
+  # replace forward slash
+  x <- gsub("/", ".", x)
+
   # get rid of any double "__"
   x <- gsub("__", "_", x)
 
-  # replace hyphens
-  x <- gsub("-", "_", x)
+  # get rid of ending "_"
+  x <- ifelse(substr(x, nchar(x), nchar(x))=="_", substr(x, 1, (nchar(x))-1), x)
+
+  # get rid of "._"
+  #x <- gsub("._", "_", x)
+
+  # get rid of "_."
+  #x <- gsub("_.", "_", x)
 
   return(x)
 }
